bd3c5261f20e5e43d4867e9194b1e6bf30015d87
chc <106435999+hsiu-chan@users.noreply.github.com>
2022-12-28 21:08:09 +0800

    modified md.js in Hyper_md

--- test/Hyper_md/md.js
+++ test/Hyper_md/md.js
@@ -1,37 +1,9 @@
-//判斷用戶
-var getUrlString = location.href;//取得網址
-var url = new URL(getUrlString);
-if (url.searchParams.get('id')=='3i'){
-  var home = document.createElement("div");
-  home.setAttribute('class', "btn");
-  home.innerHTML = "<i class=\"fa-solid fa-angles-left\"></i>";
-  home.style = "top:8px;left:8px;";
-  home.setAttribute('herf', "../index.html");
-  home.addEventListener('click', function() {
-    location.href='../index.html';
-  });
-  document.body.appendChild(home);
-}
 
-/*window.onload=function(){
-  var head=document.getElementsByTagName('head')[0]; //获取到head元素
-  var link=document.createElement('link');//创建link元素节点，也就是link标签
-  link.rel="stylesheet"; //为link标签添加rel属性
-  link.href="Hyper_md/md.css";//为link标签添加href属性 ， 属性值是css外链样式表的路径
-  head.appendChild(link);//将link元素节点添加到head元素子节点下
-  
-  var script=document.createElement('script');//创建script标签
-  //script.crossorigin="anonymous"
-  script.src="https://kit.fontawesome.com/849ddf9236.js";
-  script.setAttribute('crossorigin', 'anonymous');
-  //head.appendChild(script);//将script标签添加到head的子节点下
-}*/
 
 
 window.onload=function(){
   var head=document.getElementsByTagName('head')[0];
   var script=document.createElement('script');
-  //script.crossorigin="anonymous"
   script.innerHTML=`var Main_LeftListLiSelector = $(".md-sidebar-toc li");
 $(Main_LeftListLiSelector).on('click', function() {
 $(Main_LeftListLiSelector).removeClass('active');
